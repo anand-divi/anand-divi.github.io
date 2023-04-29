6ccb9e0fc94c0bc5723f92e65b6522d0131afb83
anand-divi <akhil244978@gmail.com>
2023-04-29 19:05:12 +0530

    portfoli

--- index.js
+++ index.js
@@ -1,175 +1,41 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
 
-/*===== MENU SHOW =====*/
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  
+  var myNav = document.getElementById('nav-menu');
+  var myNav2 = document.getElementById('main');
+  window.onscroll = function () { 
+      if ( document.documentElement.scrollTop >= 15 ) {
+          myNav.classList.add("nav-colored");
+          myNav2.classList.add("nav-colored");
+          // myNav.classList.remove("nav-transparent");
+      } 
+      else {
+          // myNav.classList.add("nav-transparent");
+          myNav.classList.remove("nav-colored");
+          myNav2.classList.remove("nav-colored");
+      }
+  };
+
+  GitHubCalendar(".calendar", "Rubel011", {
+    responsive: true,
+    global_stats: false,
+    tooltips: true,
   });
-}
 
-/*===== MENU HIDDEN =====*/
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
-}
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll("[data-content]");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
-
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("qualification__active");
-    });
-    target.classList.add("qualification__active");
-
-    tabs.forEach((tab) => {
-      tab.classList.remove("qualification__active");
-    });
-    tab.classList.add("qualification__active");
-  });
-});
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader() {
-  const nav = document.getElementById("header");
-  if (this.scrollY >= 80) nav.classList.add("scroll-header");
-  else nav.classList.remove("scroll-header");
-}
-window.addEventListener("scroll", scrollHeader);
-
-/*==================== SHOW SCROLL UP ====================*/
-function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollUp);
-
-/*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
-  );
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
-});
-
-
-
-/*==================== FORM SUBMIT ====================*/
-document.getElementById("contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  //button animation
-  document.getElementById("email-submit").innerText = "Sending...";
-
-  if (
-    document.getElementById("name").value != "" &&
-    document.getElementById("email").value != "" &&
-    document.getElementById("message").value != ""
-  ) {
-    let params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_embce24";
-    const templateID = "template_cci0niw";
-
-    emailjs
-      .send(serviceID, templateID, params)
-      .then((res) => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-
-        document.getElementById("email-submit").innerHTML = `
-          Send message
-          <i class="uil uil-message button__icon"></i>`;
-
-        alert("Your message send susscessfully!");
-      })
-      .catch((err) => {
-        document.getElementById("email-submit").innerHTML = `
-          Send message
-          <i class="uil uil-message button__icon"></i>`;
-        console.log(err);
-      });
-  } else {
-    alert("Please fill out all the fields");
+  document.getElementById("resume-button-1").onclick=()=>{
+window.open("https://drive.google.com/file/d/1gaYg_spHeDyU9UGdazWoWeSvwVrNAN4k/view");
   }
-});
 
-/*==================== GITHUB CALENDAR ====================*/
-GitHubCalendar(".calendar", "anand-divi", {
-  responsive: true,
-  global_stats: false,
-  tooltips: true,
-});
-
+  document.getElementById("resume-button-2").onclick=()=>{
+    window.open("https://drive.google.com/file/d/1gaYg_spHeDyU9UGdazWoWeSvwVrNAN4k/view");
+  }
+     
+  
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.display= "block";
+  }
